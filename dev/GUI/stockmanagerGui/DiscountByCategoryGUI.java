8d2b2afaff3c52d4acc7af5fe6c35d73f5e01e24
YUval <yuvals249@gmail.com>
2023-06-14 16:40:32 +0300

    added advance version to class DiscountByCategoryGUI

--- dev/GUI/stockmanagerGui/DiscountByCategoryGUI.java
+++ dev/GUI/stockmanagerGui/DiscountByCategoryGUI.java
@@ -7,12 +7,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-public class DiscountByCategoryGUI extends JPanel{
+public class DiscountByCategoryGUI extends JPanel {
 
     private JPanel mainPanel;
     private MarketMenuGui parent;
 
-    public DiscountByCategoryGUI(MarketMenuGui parent){
+    public DiscountByCategoryGUI(MarketMenuGui parent) {
         this.parent = parent;
         setLayout(new BorderLayout());
 
@@ -25,97 +25,116 @@ public class DiscountByCategoryGUI extends JPanel{
         }
 
         Image finalBackground = background;
-        mainPanel = new JPanel(){
+        mainPanel = new JPanel() {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.drawImage(finalBackground, 0, 0, getWidth(), getHeight(), this);
             }
         };
         mainPanel.setLayout(new BorderLayout());
-        JLabel titleLabel = new JLabel("<html>Discount By Category</html>");
+        JLabel titleLabel = new JLabel("<html>Discount By Category <br><br> </html>");
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
         titleLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
         mainPanel.add(titleLabel, BorderLayout.NORTH);
-        mainPanel.setLayout(new FlowLayout());
 
+
+        // Create text fields
+        JLabel categoryLabel = new JLabel("Category");
+        Font categoryLabelFont = categoryLabel.getFont();
+        Font categoryLabelNewFont = categoryLabelFont.deriveFont(Font.PLAIN, 20);
+        categoryLabel.setFont(categoryLabelNewFont);
+
+        JTextField categoryTextField = new JTextField();
+        Font categoryTextFieldFont = categoryTextField.getFont();
+        Font categoryTextFieldNewFont = categoryTextFieldFont.deriveFont(Font.PLAIN, 20);
+        categoryTextField.setFont(categoryTextFieldNewFont);
+
+
+        JLabel subCategoryLabel = new JLabel("Sub Category");
+        Font subCategoryLabelFont = subCategoryLabel.getFont();
+        Font subCategoryLabelNewFont = subCategoryLabelFont.deriveFont(Font.PLAIN, 20);
+        subCategoryLabel.setFont(subCategoryLabelNewFont);
+
+        JTextField subCategoryTextField = new JTextField();
+        Font subCategoryTextFieldFont = subCategoryTextField.getFont();
+        Font subCategoryTextFieldNewFont = subCategoryTextFieldFont.deriveFont(Font.PLAIN, 20);
+        subCategoryTextField.setFont(subCategoryTextFieldNewFont);
+
+
+        JLabel subSubCategoryLabel = new JLabel("Sub Sub Category");
+        Font subSubCategoryLabelFont = subSubCategoryLabel.getFont();
+        Font subSubCategoryLabelNewFont = subSubCategoryLabelFont.deriveFont(Font.PLAIN, 20);
+        subSubCategoryLabel.setFont(subSubCategoryLabelNewFont);
+
+        JTextField subSubCategoryTextField = new JTextField();
+        Font subSubCategoryTextFieldFont = subSubCategoryTextField.getFont();
+        Font subSubCategoryTextFieldNewFont = subSubCategoryTextFieldFont.deriveFont(Font.PLAIN, 20);
+        subSubCategoryTextField.setFont(subSubCategoryTextFieldNewFont);
+
+
+        JLabel discountLabel = new JLabel("Discount");
+        Font discountLabelFont = discountLabel.getFont();
+        Font discountLabelNewFont = discountLabelFont.deriveFont(Font.PLAIN, 20);
+        discountLabel.setFont(discountLabelNewFont);
+
+        JTextField discountTextField = new JTextField();
+        Font discountTextFieldFont = discountTextField.getFont();
+        Font discountTextFieldNewFont = discountTextFieldFont.deriveFont(Font.PLAIN, 20);
+        discountTextField.setFont(discountTextFieldNewFont);
+
+
+        JPanel inputPanel = new JPanel();
+        int verticalGap = 35; // Set the desired vertical gap between rows
+        int horizontalGap = 20;
+        inputPanel.setLayout(new GridLayout(4, 2, horizontalGap, verticalGap));
+        inputPanel.add(categoryLabel);
+        inputPanel.add(categoryTextField);
+        inputPanel.add(subCategoryLabel);
+        inputPanel.add(subCategoryTextField);
+        inputPanel.add(subSubCategoryLabel);
+        inputPanel.add(subSubCategoryTextField);
+        inputPanel.add(discountLabel);
+        inputPanel.add(discountTextField);
+
+        inputPanel.setOpaque(false);
+
+//        mainPanel.add(inputPanel, BorderLayout.CENTER);
+        JPanel inputWrapperPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        inputWrapperPanel.add(inputPanel);
+        mainPanel.add(inputWrapperPanel, BorderLayout.WEST);
+
+        inputWrapperPanel.setOpaque(false);
 
         // Create button panel
+        JButton submitButton = new JButton("Submit");
+        //JButton submitButton = createButton("Submit", "/GUI/pictures/stock-manager.jpg");
         JButton backButton = new JButton("Back");
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
-        buttonPanel.setOpaque(false);
 
-        // Create buttons
-        try {
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        buttonsPanel.add(submitButton);
+        buttonsPanel.add(backButton);
 
-            JPanel texts = new JPanel();
-            texts.setLayout(new BoxLayout(texts, BoxLayout.Y_AXIS));
+        buttonsPanel.setOpaque(false);
 
-            JLabel categoryLabel = new JLabel("Category");
-            JLabel subCategoryLabel = new JLabel("Sub Category");
-            JLabel subSubCategoryLabel = new JLabel("Sub Sub Category");
-            JLabel discountLabel = new JLabel("Discount");
+        mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
 
-            JTextField category = new JTextField();
-            category.setColumns(15);
-            JTextField subCategory = new JTextField();
-            subCategory.setColumns(15);
-            JTextField subSubCategory = new JTextField();
-            subSubCategory.setColumns(15);
-            JTextField discount = new JTextField();
-            discount.setColumns(15);
+        add(mainPanel, BorderLayout.CENTER);
 
-            texts.add(createTextFieldPanel(categoryLabel, category));
-            texts.add(createTextFieldPanel(subCategoryLabel, subCategory));
-            texts.add(createTextFieldPanel(subSubCategoryLabel, subSubCategory));
-            texts.add(createTextFieldPanel(discountLabel, discount));
-            texts.setVisible(true);
-            mainPanel.add(texts);
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Handle submit button action
+            }
+        });
 
-            JButton submit = createButton("Submit", "/GUI/pictures/stock-manager.jpg");
-
-
-            buttonPanel.add(Box.createHorizontalGlue());
-            buttonPanel.add(submit);
-            buttonPanel.add(Box.createHorizontalGlue());
-
-            JPanel bottomPanel = new JPanel();
-            bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-            bottomPanel.add(backButton);
-
-            // Add button panel to the main panel
-            mainPanel.add(Box.createVerticalStrut(120)); // Adjust the spacing as needed
-            mainPanel.add(buttonPanel,BorderLayout.CENTER);
-
-
-            mainPanel.add(Box.createVerticalStrut(200));
-            mainPanel.add(bottomPanel, BorderLayout.SOUTH);
-
-
-            add(mainPanel, BorderLayout.CENTER);
-            mainPanel.setVisible(true);
-
-
-            submit.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-//                parent.showDefaultPanelFromChild();
-                }
-            });
-
-
-            backButton.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    parent.showDefaultPanelFromChild();
-                }
-            });
-
-        }catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-
+        backButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                parent.showDefaultPanelFromChild();
+            }
+        });
 
     }
+
+
     private JButton createButton(String text, String imagePath) throws IOException {
         // Create button panel
         int width = 100;
@@ -130,7 +149,7 @@ public class DiscountByCategoryGUI extends JPanel{
         Image small_image = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
         ImageIcon imageIcon = new ImageIcon(small_image);
         imageLabel.setIcon(imageIcon);
-        imageLabel.setBounds(0,0,width,height);
+        imageLabel.setBounds(0, 0, width, height);
         imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
         buttonPanel.add(imageLabel, BorderLayout.CENTER);
 
@@ -166,13 +185,13 @@ public class DiscountByCategoryGUI extends JPanel{
     private void removeCurrentChildPanel() {
 
     }
+
     private JPanel createTextFieldPanel(JLabel label, JTextField textField) {
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(label, BorderLayout.WEST);
         panel.add(textField, BorderLayout.CENTER);
         return panel;
     }
-
 
 
 }
